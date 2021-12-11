e1083ce1941754612fc99f05d60d6f8fa4a495ae
upm1 <72426046+upm1@users.noreply.github.com>
2021-12-10 23:05:01 -0600

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,21 +1,21 @@
 const products = [{
         name: "sanwich",
-        src: "../assets/sanwich.jpg",
+        src: "assets/sanwich.jpg",
         price: "$252.99"
     },
     {
         name: "hot-cake",
-        src: "../assets/cake.jpg",
+        src: "assets/cake.jpg",
         price: "$589.99"
     },
     {
         name: "chocolate",
-        src: "../assets/chocolate.jpg",
+        src: "assets/chocolate.jpg",
         price: "$1099.99"
     },
     {
         name: "dulce",
-        src: "../assets/dulce.jpg",
+        src: "assets/dulce.jpg",
         price: "$2449.99"
     }
 ];
